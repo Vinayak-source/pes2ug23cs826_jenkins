fc6c2ceb47a50d0aff26d4cebbcefeed1b2bfe37
Vinayak-source <vinayak.domain@gmail.com>
2025-03-10 22:21:38 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -4,6 +4,7 @@ using namespace std;
 int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
-  cout << "hi mommy";
+  cout << "hi mommy\n";
+  cout << "completed CC Lab \n";
   return 0;
 }
